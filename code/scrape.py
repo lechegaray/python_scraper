46feadff3cdc99ec2fe0cf8623671d014083a10f
Luis Echegaray <mrluisechegaray@gmail.com>
2014-12-18 23:27:33 -0500

    import cvs, save to outputfile

--- code/scrape.py
+++ code/scrape.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
@@ -8,6 +9,14 @@ html = response.content
 soup = BeautifulSoup(html)
 table = soup.find('table', attrs={'class': 'resultsTable'})
 
+list_of_rows = []
 for row in table.findAll('tr'):
+	list_of_cells = []
 	for cell in row.findAll('td'):
-		print cell.text
+		text = cell.text.replace('&nbsp;', '')
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+outfile = open("./inmates.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerows(list_of_rows)
